78d91920dacc2a1b036d331e59642f3ddfac802c
Ruby Rinken <rubyrinken@gmail.com>
2020-05-06 13:54:34 -0600

    Allow user to filter articles by title and description

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import entertainment from '../../data/entertainment';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu';
-// import SearchForm from '../SearchForm/SearchForm';
+import SearchForm from '../SearchForm/SearchForm';
 
 class App extends Component {
   constructor() {
@@ -18,7 +18,8 @@ class App extends Component {
       science,
       technology,
       entertainment,
-      currentTab: "local"
+      currentTab: "local",
+      filter: ""
     }
   }
 
@@ -28,13 +29,21 @@ class App extends Component {
     });
   }
 
+  filterArticles(searchInput) {
+    this.setState({
+      filter: searchInput
+    });
+  }
+
   render () {
     return (
-      <div className="app">
-        {/* <SearchForm /> */}
-        <Menu selected={this.state.currentTab} pickTopic={this.pickTopic.bind(this)} />
-        <NewsContainer articles={this.state[this.state.currentTab]}/>
-      </div>
+      <React.Fragment>
+        <SearchForm search={this.filterArticles.bind(this)}/>
+        <div className="app">
+          <Menu selected={this.state.currentTab} pickTopic={this.pickTopic.bind(this)} />
+          <NewsContainer articles={this.state[this.state.currentTab]} filter={this.state.filter}/>
+        </div>
+      </React.Fragment>
     );
   }
 }
